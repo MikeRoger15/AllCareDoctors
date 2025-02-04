9e026e21113faa1c47b2cabdc693b5b45b20737d
Muneeb Mastan <muneeb.mastan@jerseystem.org>
2025-02-04 13:21:12 -0500

    Internl and family Doctor Pages

--- WebApplication1/AllCareDoctors/Controllers/PrimaryCareControllers/PrimaryCareController.cs
+++ WebApplication1/AllCareDoctors/Controllers/PrimaryCareControllers/PrimaryCareController.cs
@@ -4,6 +4,9 @@ namespace AllCareDoctors.Controllers.PrimaryCareControllers;
 
 public class PrimaryCareController : Controller
 {
+    
+    public IActionResult PrimaryCare() => View("~/Views/CareAndServices/PrimaryCare.cshtml");
     public IActionResult InternalMedicine() => View("~/Views/CareAndServices/Internal-Medicine.cshtml");
+    
     public IActionResult FamilyMedicine() => View("~/Views/CareAndServices/Family-Medicine.cshtml");
 }
