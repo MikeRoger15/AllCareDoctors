fe61b9441610133be22bc5eb891e72e185adcd26
KevinSupaano <KevinSupaano@yahoo.com>
2025-02-12 13:44:49 -0500

    Moved files to its appropriate folders, renamed files to one standard naming structure, edited multiple cshtml, css, c# and js files

---
+++ WebApplication1/AllCareDoctors/Controllers/AccountControllers/AccountController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace AllCareDoctors.Controllers.AccountControllers;
+
+public class AccountController: Controller
+{
+    public IActionResult Login() => View("~/Views/Account/Login.cshtml");
+    public IActionResult Registration() => View("~/Views/Account/Registration.cshtml");
+    public IActionResult PostLogin() => View("~/Views/Account/PostLoginPage.cshtml");
+    
+}
--- WebApplication1/AllCareDoctors/Controllers/PrimaryCareControllers/PrimaryCareController.cs
+++ WebApplication1/AllCareDoctors/Controllers/PrimaryCareControllers/PrimaryCareController.cs
@@ -5,8 +5,12 @@ namespace AllCareDoctors.Controllers.PrimaryCareControllers;
 public class PrimaryCareController : Controller
 {
     
-    public IActionResult PrimaryCare() => View("~/Views/CareAndServices/PrimaryCare.cshtml");
-    public IActionResult InternalMedicine() => View("~/Views/CareAndServices/Internal-Medicine.cshtml");
+    public IActionResult PrimaryCare() => View("~/Views/CareAndServices/PrimaryCare/PrimaryCare.cshtml");
+    public IActionResult InternalMedicine() => View("~/Views/CareAndServices/PrimaryCare/InternalCare/Internal-Medicine.cshtml");
+    public IActionResult InternalDoc1() => View("~/Views/CareAndServices/PrimaryCare/InternalCare/InternalDoctor1.cshtml");
+    public IActionResult InternalDoc2() => View("~/Views/CareAndServices/PrimaryCare/InternalCare/InternalDoctor2.cshtml");
     
-    public IActionResult FamilyMedicine() => View("~/Views/CareAndServices/Family-Medicine.cshtml");
+    public IActionResult FamilyMedicine() => View("~/Views/CareAndServices/PrimaryCare/FamilyCare/Family-Medicine.cshtml");
+    public IActionResult FamilyDoc1() => View("~/Views/CareAndServices/PrimaryCare/FamilyCare/FamilyDoctor1.cshtml");
+    public IActionResult FamilyDoc2() => View("~/Views/CareAndServices/PrimaryCare/FamilyCare/FamilyDoctor2.cshtml");
 }
